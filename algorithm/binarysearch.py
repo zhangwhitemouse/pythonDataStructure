63aa6983e8bd98b2da1492ee5b3fa86c8f69836e
zhang <slg__zhanglin@163.com>
2020-08-21 09:52:29 +0800

    重写二分排序

--- algorithm/binarysearch.py
+++ algorithm/binarysearch.py
@@ -3,23 +3,27 @@
 二分查找算法
 """
 
-def binary_search(array,item):
-    low = 0
-    high = len(array) - 1
+def binary_search(arr,item):
+    """
+    二分查找,arr数组必须有序
+    :param arr: 查找的数组
+    :param item: 带查找的数
+    :return: item在arr中的索引
+    """
+    left = 0
+    right = len(arr) - 1
 
-    while low <= high:
-        mid = (low + high) // 2
+    #这里=号必须得加，否则边界会有问题
+    while left <= right:
+        mid = (left + right) // 2
 
-        guess = array[mid]
-
-        if guess == item:
-            return mid
-        if guess > item:
-            high = mid -1
+        if arr[mid] > item:
+            right = mid - 1
+        elif arr[mid] < item:
+            left = mid + 1
         else:
-            low = mid + 1
+            return mid
+
     return None
 
-
 print(binary_search([1,3,5,7,9],9))
-        

465a0c77c186c52a5537722ff4af4f038c37a5c9
zhang <slg__zhanglin@163.com>
2020-12-24 20:48:03 +0800

    全部滤波代码

---
+++ paper/clahe.py
@@ -0,0 +1,20 @@
+import cv2
+
+mri_img = cv2.imread('gray.png')
+
+lab = cv2.cvtColor(mri_img, cv2.COLOR_BGR2LAB)
+
+lab_planes = cv2.split(lab)
+
+clahe = cv2.createCLAHE(clipLimit=10,tileGridSize=(8,8))
+
+lab_planes[0] = clahe.apply(lab_planes[0])
+
+lab = cv2.merge(lab_planes)
+
+bgr = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)
+cv2.namedWindow("p",0)
+cv2.resizeWindow("p",640,480)
+cv2.imwrite('clahe.png',bgr)
+cv2.imshow('p',bgr)
+cv2.waitKey(0)
